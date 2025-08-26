edf07d253ea66bef6d92b3b78d123ece745525e0
Paul De Palma <depalma@gonzaga.edu>
2025-08-25 22:42:44 -0700

    commit 3

---
+++ ex4.py
@@ -0,0 +1,28 @@
+'''
+Demonstrates:
+1. random integers 
+2. Conditional statements 
+'''
+
+import random
+
+def do_stuff(size):
+   lst = [random.randint(0,10) for i in range(10)]
+   return lst
+
+def main():
+   howMany = 10
+   lst = do_stuff(howMany)
+   num = random.randint(0,10)
+   print("Num = " + str(num))
+   if num in lst:
+      print(True)
+   else:
+      print(False)
+
+   #More pythonically
+   print(num in lst)
+   print(lst)
+
+if __name__ == "__main__":
+   main()
