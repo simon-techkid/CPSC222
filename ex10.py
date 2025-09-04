420bdf7ea5d26b165df1ad78c8469475943edf52
Paul De Palma <depalma@gonzaga.edu>
2025-09-03 22:40:47 -0700

    commit

--- ex10.py
+++ ex10.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 #plt.style.use('classic')
 
 def main():
+    pl = plt.gca()  #get current axis for the subplot
     x = [i for i in range(-100,100)]
     y = [i*i for i in x] 
     plt.plot(x,y)
--- ex11.py
+++ ex11.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 
 import math
 
-
 def plot_zipf(x,y,xx,yy):
-
-    pl = plt.subplot(1,1,1)
-    pl.plot(x,y)
+    pl = plt.gca()
+    #pl.plot(x,y)
+    pl.plot(xx,yy)
     pl.set_xlabel("Rank")
     pl.set_ylabel("Probability")
     #pl.plot("b",x,y)
@@ -16,7 +15,6 @@ def plot_zipf(x,y,xx,yy):
         
    
 def main():
-
         x = [i for i in range (1,500)]
         y = [1/i for i in x]
         xx = [math.log(i) for i in x]
